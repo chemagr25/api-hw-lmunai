96c9736fc4e1086fa0bb192059fa246c6656dffd
chemagr25 <chemagr25@gmail.com>
2023-06-23 08:40:56 -0600

    api

---
+++ controllers/BookController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\controllers;
+
+class BookController extends \yii\rest\ActiveController
+{
+
+    public $modelClass = 'app\models\Book';
+    
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
---
+++ models/Book.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "book".
+ *
+ * @property int $id
+ * @property string|null $name
+ * @property string|null $author
+ * @property int|null $release_year
+ * @property int|null $is_available_for_loan
+ */
+class Book extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'book';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['release_year', 'is_available_for_loan'], 'integer'],
+            [['name', 'author'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'author' => 'Author',
+            'release_year' => 'Release Year',
+            'is_available_for_loan' => 'Is Available For Loan',
+        ];
+    }
+}
